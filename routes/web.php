cc5978264074e0d1087705312128fd6f94401f60
shinito <info@shinito.net>
2019-10-23 15:17:39 +0900

    ch08

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
+Route::get('/', 'HomeController@index')->name('home');
+
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
 
 Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
@@ -21,3 +23,5 @@ Route::post('/folders/{id}/tasks/create','TaskController@create');
 
 Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
 Route::post('/folders/{id}/tasks/{task_id}/edit','TaskController@edit');
+
+Auth::routes();
